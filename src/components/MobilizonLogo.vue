9116647ee86417396a103692365531d126a87ad5
joe <joe@example.org>
2025-06-20 17:19:08 +0000

    Use configured logo (again)

--- src/components/MobilizonLogo.vue
+++ src/components/MobilizonLogo.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1>Klimatkalendern</h1>
+  <h1 v-if="!instanceLogoUrl">Klimatkalendern</h1>
+  <img v-else alt="" class="max-h-12 w-auto" :src="instanceLogoUrl" />
 </template>
+
+<script lang="ts" setup>
+import { useInstanceLogoUrl } from "@/composition/apollo/config";
+const { instanceLogoUrl } = useInstanceLogoUrl();
+</script>
