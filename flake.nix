ff330519df4b4cd77a3902fb4e7b2adc7144559c
Alexander Holmbäck <alex@ahbk.se>
2025-07-31 11:43:36 +0200

    infra: add forAllSystems to flake.nix

--- flake.nix
+++ flake.nix
@@ -14,52 +14,67 @@
       pname = "klimatkalendern";
       version = "5.1.2";
       src = ./.;
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      elixirPackage = pkgs.beam.packages.erlang_26.elixir_1_15;
-      beamPackages = pkgs.beam.packages.erlang_26.extend (
-        self: super: {
-          elixir = self.elixir_1_15;
-        }
-      );
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in
     rec {
-      packages."aarch64-darwin".default = packages.${system}.default;
-      packages.${system}.default = pkgs.callPackage ./nixpkg {
-        mobilizon-src = {
-          inherit src pname version;
-        };
-        elixir = elixirPackage;
-        inherit beamPackages;
-        mobilizon-frontend = pkgs.callPackage ./nixpkg/frontend.nix {
-          mobilizon-src = {
-            inherit src pname version;
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          elixirPackage = pkgs.beam.packages.erlang_26.elixir_1_15;
+          beamPackages = pkgs.beam.packages.erlang_26.extend (
+            self: super: {
+              elixir = self.elixir_1_15;
+            }
+          );
+        in
+        {
+          default = pkgs.callPackage ./nixpkg {
+            mobilizon-src = {
+              inherit src pname version;
+            };
+            elixir = elixirPackage;
+            inherit beamPackages;
+            mobilizon-frontend = pkgs.callPackage ./nixpkg/frontend.nix {
+              mobilizon-src = {
+                inherit src pname version;
+              };
+            };
           };
-        };
-      };
+        }
+      );
 
-      nixosModules.mobilizon = { };
-
-      devShells.${system} = {
-        default = pkgs.mkShell {
-          name = "${pname}-dev";
-          packages = with pkgs; [
-            mix2nix
-            elixirPackage
-            cmake
-            nodejs
-            unzip
-            openssl
-            file
-            imagemagick
-            libwebp
-            gifsicle
-            jpegoptim
-            optipng
-            pngquant
-            inotify-tools
-          ];
-        };
-      };
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          elixirPackage = pkgs.beam.packages.erlang_26.elixir_1_15;
+        in
+        {
+          default = pkgs.mkShell {
+            name = "${pname}-dev";
+            packages = with pkgs; [
+              mix2nix
+              elixirPackage
+              cmake
+              nodejs
+              unzip
+              openssl
+              file
+              imagemagick
+              libwebp
+              gifsicle
+              jpegoptim
+              optipng
+              pngquant
+              inotify-tools
+            ];
+          };
+        }
+      );
     };
 }
