48addc2c94a5d8a55bcdded08da8c722b2d1e42f
Laurent Gay <laurent@sleto.fr>
2025-09-19 16:17:47 +0200

    migration about user role "pending" - #877

--- priv/repo/migrations/20250911120910_add_moderation_to_users.exs
+++ priv/repo/migrations/20250911120910_add_moderation_to_users.exs
@@ -1,9 +1,13 @@
 defmodule Mobilizon.Storage.Repo.Migrations.AddModerationToUser do
   use Ecto.Migration
 
+  alias Mobilizon.Users.UserRole
+
   def change do
     alter table(:users) do
       add(:moderation, :string, default: "")
     end
+
+    UserRole.create_type()
   end
 end
