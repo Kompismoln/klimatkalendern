023b791577cd80691c1b62040c176ddd64339d0b
Simon <contact@smn.codes>
2026-01-20 12:48:13 +0100

    refactor: style calendar export 'card'

--- src/views/Event/CalendarView.vue
+++ src/views/Event/CalendarView.vue
@@ -4,21 +4,31 @@
       {{ t("Calendar") }}
     </h1>
 
-    <h2>
-	    {{ t("Exporting events") }}
-    </h2>
-    <p>
-		    {{ t( "Press this button to export evenets to your calendar app. "+
-		    "You can choose which municipalities to include, "+
-		    "and when new events are posted you will receive updates."
-		    ) }}
-		    <br />
-	    <a href="https://klimatkalendern.vercel.app/">
-		    <o-button>
-		    {{ t("Export events to your calendar") }}
-		    </o-button>
-	    </a>
-    </p>
+    <div class="mbz-card mb-4 overflow-hidden dark:border-gray-700 shadow-md dark:bg-mbz-purple md:flex">
+      <img class="w-full md:max-w-80 md:pr-3 hidden md:block" src="/img/klimatkalendern-city.jpg" alt="">
+      <div class="p-4 pb-6 flex flex-col items-start gap-3">
+        <h2 class="font-extrabold">
+          {{ t("Exporting events") }}
+        </h2>
+        <p>
+          {{ t( "Press this button to export evenets to your calendar app. "+
+          "You can choose which municipalities to include, "+
+          "and when new events are posted you will receive updates."
+          ) }}
+        </p>
+        <o-button
+          tag="a"
+          href="https://klimatkalendern.vercel.app/"
+          iconLeft="Calendar"
+          iconRight="OpenInNew"
+          class="!h-auto"
+          target="_blank"
+          rel="noopener noreferer"
+        >
+          {{ t(isMobile ? "Export events" : "Export events to your calendar") }}
+        </o-button>
+      </div>
+    </div>
 
     <div class="p-2">
       <EventsCalendar v-if="!isMobile" />
@@ -27,6 +37,7 @@
   </div>
 </template>
 <script lang="ts" setup>
+import { ref } from "vue";
 import { useI18n } from "vue-i18n";
 import EventsAgenda from "@/components/FullCalendar/EventsAgenda.vue";
 import EventsCalendar from "@/components/FullCalendar/EventsCalendar.vue";
@@ -42,9 +53,11 @@ onMounted(() => {
     "//span[contains(@class,'fc-icon-chevron-right')]"
   );
   icon_right.setAttribute("aria-label", t("Next"));
+
+  window.onresize = () => isMobile.value = window.innerWidth < 760;
 });
 
 const { t } = useI18n({ useScope: "global" });
 
-const isMobile = window.innerWidth < 760;
+const isMobile = ref(window.innerWidth < 760);
 </script>
