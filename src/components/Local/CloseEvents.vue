be87bbffb7c6ed23544af4794e4ddf3fd9a0b1df
Ludvig Janiuk <ludvig.janiuk@proton.me>
2026-07-23 16:35:57 +0200

    Small fix, target of see more card

--- src/components/Local/CloseEvents.vue
+++ src/components/Local/CloseEvents.vue
@@ -39,7 +39,8 @@
         v-show="loading"
       />
       <span v-for="event in events.elements" :key="event.elem.uuid">
-        <event-card :event="event.elem" :key="event.elem.uuid" :viewMoreFrom="event.showActorMore" />
+        <event-card v-if="event.showActorMore" :event="event.elem" :key="event.elem.uuid" :viewMoreFrom="event.showActorMore" :to="event.actorMoreRoute" />
+        <event-card v-else :event="event.elem" :key="event.elem.uuid" :viewMoreFrom="event.showActorMore" />
       </span>
       <more-content
         v-if="userLocation?.name && userLocation?.lat && userLocation?.lon"
